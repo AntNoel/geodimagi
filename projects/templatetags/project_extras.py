a137be72b492be52c06025832a90c52bc617d576
Anthony Noel <antjnoel@gmail.com>
2022-12-20 00:12:36 -0600

    Corrected custom filter setup

--- projects/templatetags/project_extras.py
+++ projects/templatetags/project_extras.py
@@ -1,8 +1,10 @@
 from django import template
+from projects.models import Project
 
 register = template.Library()
 
 
 @register.filter()
 def division_filter(model, divisions):
-    return model.all.filter(team_division__in=divisions)
+
+    return Project.objects.filter(team_division__in=divisions)
