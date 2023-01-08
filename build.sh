03fcaa9680473f3d13703df0a1d150daf3362641
Anthony Noel <antjnoel@gmail.com>
2023-01-08 16:52:03 -0600

    Additional steps added to build file for installing virtual environment and installing packages

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -o errexit
 
 virtualenv install
+python -m venv myvenv
+source myvenv/bin/activate
+install requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
