ce0368108ae6f48d6cebe64592969bd57979a6da
Anthony Noel <antjnoel@gmail.com>
2023-01-08 16:55:23 -0600

    Added pip installation to build file

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 virtualenv install
 python -m venv myvenv
 source myvenv/bin/activate
-install requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
