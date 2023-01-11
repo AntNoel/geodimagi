f6cbc20ebf3e132a66d8650d89c447b227a6bc74
Anthony Noel <antjnoel@gmail.com>
2023-01-11 17:15:14 -0600

    Reverted some pre-deployment changes to use local server

--- geodimagi/settings.py
+++ geodimagi/settings.py
@@ -108,22 +108,22 @@ WSGI_APPLICATION = "geodimagi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.contrib.gis.db.backends.postgis",
-#         "NAME": "geodimagi",
-#         "USER": "geodimagiadmin",
-#         "PASSWORD": "geodimagi",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "geodimagi",
+        "USER": "geodimagiadmin",
+        "PASSWORD": "geodimagi",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 
-DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
+# DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
-# GDAL_LIBRARY_PATH = r"C:\Program Files\QGIS 3.28.1\bin\gdal306"
-# GEOS_LIBRARY_PATH = r"C:\Program Files\QGIS 3.28.1\bin\geos_c"
+GDAL_LIBRARY_PATH = r"C:\Program Files\QGIS 3.28.1\bin\gdal306"
+GEOS_LIBRARY_PATH = r"C:\Program Files\QGIS 3.28.1\bin\geos_c"
 
 
 # Password validation
