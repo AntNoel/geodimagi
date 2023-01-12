8b90a9d356ca0f24e39003063fb9a8adc06d3eea
Anthony Noel <antjnoel@gmail.com>
2023-01-12 11:28:28 -0600

    Removed comments in settings

--- geodimagi/settings.py
+++ geodimagi/settings.py
@@ -40,7 +40,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str(
     "SECRET_KEY",
 )
@@ -54,7 +53,6 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
