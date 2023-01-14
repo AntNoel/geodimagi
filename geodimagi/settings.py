fff5588d0971b678e355d7bf0165661a4d830d57
Anthony Noel <antjnoel@gmail.com>
2023-01-14 15:03:57 -0600

    Commented out dev server info

--- geodimagi/settings.py
+++ geodimagi/settings.py
@@ -108,16 +108,16 @@ WSGI_APPLICATION = "geodimagi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "geodimagi",
-        "USER": "geodimagiadmin",
-        "PASSWORD": "geodimagi",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.contrib.gis.db.backends.postgis",
+#         "NAME": "geodimagi",
+#         "USER": "geodimagiadmin",
+#         "PASSWORD": "geodimagi",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 DATABASES = {
     "default": dj_database_url.config(
