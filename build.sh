6b557512389b2c0063b747e4490cc9f348acbcda
Anthony Noel <antjnoel@gmail.com>
2023-01-22 23:43:02 -0600

    Mothergeo-py doc steps for downloading gdal

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
 #Exit on error
 set -o errexit
 
-install binutils libproj-dev python-gdal
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+pip install GDAL
 
 # pip install gdal-bin libgdal-dev
 # pip aptitude install python3-gdalinstall python3-gdal
