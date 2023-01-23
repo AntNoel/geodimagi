d7086a13c0378b85ea9038e1885a26a01d6e8708
Anthony Noel <antjnoel@gmail.com>
2023-01-22 23:23:51 -0600

    single line to install geospatial librariess

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #Exit on error
 set -o errexit
 
-pip install gdal-bin libgdal-dev
-pip aptitude install python3-gdalinstall python3-gdal
-pip aptitude install binutils libproj-dev
+install binutils libproj-dev gdal-bin
+
+# pip install gdal-bin libgdal-dev
+# pip aptitude install python3-gdalinstall python3-gdal
+# pip aptitude install binutils libproj-dev
 
 pip install -r requirements.txt
 
