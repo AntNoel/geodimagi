adc80f8d529e1dbcdda5e0fe8e9b2d4476599152
Anthony Noel <antjnoel@gmail.com>
2023-02-11 17:14:18 -0600

    Commented out pip statements

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 
-pip install gdal-bin libgdal-dev
-pip aptitude install python3-gdalinstall python3-gdal
-pip aptitude install binutils libproj-dev
+# pip install gdal-bin libgdal-dev
+# pip aptitude install python3-gdalinstall python3-gdal
+# pip aptitude install binutils libproj-dev
 
 pip install -r requirements.txt
 
